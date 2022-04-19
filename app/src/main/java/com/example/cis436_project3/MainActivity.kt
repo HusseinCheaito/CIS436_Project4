0b74e34da7f9e775683acc262f51dbc08fed19ef
Bailey-Merritt <63383959+Bailey-Merritt@users.noreply.github.com>
2022-04-19 06:06:46 -0400

    Started Volley Implementation

--- app/src/main/java/com/example/cis436_project3/MainActivity.kt
+++ app/src/main/java/com/example/cis436_project3/MainActivity.kt
@@ -2,17 +2,67 @@ package com.example.cis436_project3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.*
+import com.example.cis436_project3.databinding.MainActivityBinding
 import com.example.cis436_project3.ui.main.SpinnerFragment
+import org.json.JSONArray
+import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding : MainActivityBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null)
+        {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, SpinnerFragment.newInstance())
                 .commitNow()
         }
+
+        val textView = binding.testText
+
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024)
+        val network = BasicNetwork(HurlStack())
+
+        val requestQueue = RequestQueue(cache, network).apply{
+            start()
+        }
+
+        val url = "https://thecatapi.com/v1/images/search"
+
+        val request = object: StringRequest(Request.Method.GET, url,
+            Response.Listener<String>{response -> textView.text = "%s".format(response.toString())},
+            Response.ErrorListener {error -> textView.text = "Error: %s".format(error.toString())})
+        {
+            override fun getHeaders() : MutableMap<String, String>
+            {
+                val headers = HashMap<String, String>()
+                headers["x-api-key"] = "cff029ba-ea55-493d-b527-06b4a6813175"
+                return headers
+            }
+        }
+
+        requestQueue.add(request)
+
     }
+
+
+
 }
+
