3eeadb4fc33adc38fa49b992a7c5fb41a4889e81
Hussein Cheaito <hcheaito2019@gmail.com>
2022-04-20 20:38:53 -0400

    Populated spinner with breeds from TheCatApi

--- app/src/main/java/com/example/cis436_project3/ui/main/SpinnerFragment.kt
+++ app/src/main/java/com/example/cis436_project3/ui/main/SpinnerFragment.kt
@@ -6,6 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import android.widget.ImageView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.cis436_project3.R
 import com.example.cis436_project3.databinding.SpinnerFragmentBinding
 
@@ -34,7 +40,44 @@ class SpinnerFragment : Fragment()
     {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+       var spinnerText = binding.breedDropDown
+
+        val requestQueue = Volley.newRequestQueue(getActivity()?.getApplicationContext())
+
+        val url = "https://api.thecatapi.com/v1/breeds"
+
+        val breedArray = mutableListOf<String>()
+
+        val request = object : JsonArrayRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+
+                for(i in 0 until response.length()){
+
+                    val jObject = response.getJSONObject(i)
+                    breedArray.add(jObject.getString("name"))
+
+                    }
+
+                val adapter = ArrayAdapter(requireContext(),
+                    android.R.layout.simple_spinner_item, breedArray)
+                spinnerText.adapter = adapter
+
+            },
+            Response.ErrorListener { error ->
+                spinnerText.setSelection(1)
+            })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["x-api-key"] = "cff029ba-ea55-493d-b527-06b4a6813175"
+                return headers
+            }
+
+        }
+
+        requestQueue.add(request)
     }
 
 }
