30a3305e5596b21ab8a953141dd9ae14e9830b00
Hussein Cheaito <hcheaito2019@gmail.com>
2022-04-20 21:19:55 -0400

    added better Error handling for api request, removed testing values

--- app/src/main/java/com/example/cis436_project3/ui/main/SpinnerFragment.kt
+++ app/src/main/java/com/example/cis436_project3/ui/main/SpinnerFragment.kt
@@ -7,12 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import android.widget.ImageView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
-import com.example.cis436_project3.R
 import com.example.cis436_project3.databinding.SpinnerFragmentBinding
 
 class SpinnerFragment : Fragment()
@@ -47,7 +45,7 @@ class SpinnerFragment : Fragment()
 
         val url = "https://api.thecatapi.com/v1/breeds"
 
-        val breedArray = mutableListOf<String>()
+        val breedList = mutableListOf<String>()
 
         val request = object : JsonArrayRequest(
             Request.Method.GET, url, null,
@@ -56,17 +54,22 @@ class SpinnerFragment : Fragment()
                 for(i in 0 until response.length()){
 
                     val jObject = response.getJSONObject(i)
-                    breedArray.add(jObject.getString("name"))
+                    breedList.add(jObject.getString("name"))
 
                     }
 
                 val adapter = ArrayAdapter(requireContext(),
-                    android.R.layout.simple_spinner_item, breedArray)
+                    android.R.layout.simple_spinner_item, breedList)
                 spinnerText.adapter = adapter
 
             },
             Response.ErrorListener { error ->
-                spinnerText.setSelection(1)
+
+                val errorMsg = arrayOf(error)
+
+                val adapter = ArrayAdapter(requireContext(),
+                    android.R.layout.simple_spinner_item, errorMsg)
+                spinnerText.adapter = adapter
             })
         {
             override fun getHeaders(): MutableMap<String, String> {
